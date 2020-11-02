16bd7f9dcddda73aa1873827ee6e91991ca3cf8c
2017128 <73094633+2017128@users.noreply.github.com>
2020-11-02 11:33:50 +0500

    Update App.js

--- react-app-assignment2/src/App.js
+++ react-app-assignment2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         //<img src={logo} className="App-logo" alt="logo" />
         <p>
-          Haisum "Trololo" Riaz
+          Haisum
         </p>
         <a
           className="App-link"
