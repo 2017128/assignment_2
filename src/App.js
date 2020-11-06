1809836bf616168b6e4b25992a010f5104527674
2017128 <73094633+2017128@users.noreply.github.com>
2020-11-06 11:03:21 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Haisum "Trololo" Riaz 2017128
+          Haisum "Trololo" Riaz 2017128 - assignment 3
         </p>
         <a
           className="App-link"
